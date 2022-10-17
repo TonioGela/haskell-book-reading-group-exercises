b13643e50526cbc992e35bde3a474209b9d54c41
Andrea Vallotti <andrea.vallotti@gmail.com>
2022-10-17 08:48:17 +0200

    solution to comprehension check, use pi

--- src/Chapter02.hs
+++ src/Chapter02.hs
@@ -1,3 +1,3 @@
 module Chapter02 where
 
-circleArea r = 3.14 * r * r
+circleArea r = pi * r * r
--- test/Spec/Chapter02Spec.hs
+++ test/Spec/Chapter02Spec.hs
@@ -6,10 +6,10 @@ import Test.Hspec (describe, it, shouldBe)
 spec =
   describe "Chapter 02" $ do
     it "calculates area of circle w/ radius 5" $ do
-      circleArea 5 `shouldBe` 3.14 * 5 * 5
+      circleArea 5 `shouldBe` pi * 5 * 5
     it "calculates area of circle w/ radius 10" $ do
-      circleArea 10 `shouldBe` 3.14 * 10 * 10
+      circleArea 10 `shouldBe` pi * 10 * 10
     it "calculates area of circle w/ radius 2" $ do
-      circleArea 2 `shouldBe` 3.14 * 2 * 2
+      circleArea 2 `shouldBe` pi * 2 * 2
     it "calculates area of circle w/ radius 4" $ do
-      circleArea 4 `shouldBe` 3.14 * 4 * 4
+      circleArea 4 `shouldBe` pi * 4 * 4
