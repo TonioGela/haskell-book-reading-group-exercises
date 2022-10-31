a61cb68545b1dc5cd3ad947cae3ad3bf8691641a
Andrea Vallotti <andrea.vallotti@gmail.com>
2022-10-31 11:06:46 +0100

    chapter 04 - add reference to excercises as commments

--- src/Chapter04/exercises.hs
+++ src/Chapter04/exercises.hs
@@ -6,11 +6,14 @@ also = ["Quake", "The Simons"]
 
 allAwesome = [awesome, also]
 
+-- 8.
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome x = x == reverse x
 
+-- 9.
 myAbs :: Integer -> Integer
 myAbs x = if x < 0 then (-x) else x
 
+-- 10.
 f :: (a, b) -> (c, d) -> ((b, d), (a, c))
 f ab cd = (,) (snd ab, snd cd) (fst ab, fst cd)
