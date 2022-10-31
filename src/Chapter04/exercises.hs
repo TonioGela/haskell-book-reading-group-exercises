0bed1c654a66e3f62bef8d6a4edcb445cbcb5a06
Andrea Vallotti <andrea.vallotti@gmail.com>
2022-10-31 11:16:02 +0100

    chapter 04 - add reference to correcting syntax

--- src/Chapter04/exercises.hs
+++ src/Chapter04/exercises.hs
@@ -17,3 +17,19 @@ myAbs x = if x < 0 then (-x) else x
 -- 10.
 f :: (a, b) -> (c, d) -> ((b, d), (a, c))
 f ab cd = (,) (snd ab, snd cd) (fst ab, fst cd)
+
+-- correcting syntax
+correctingSyntax1 xs = f xs
+  where
+    x = (+)
+    f xs = w `x` 1
+      where
+        w = length xs
+
+correctingSyntax2 x = f x
+  where
+    f = \x -> x
+
+correctingSyntax3 (a, b) = f (a, b)
+  where
+    f (a, b) = a
