0ba2cf847103861fe897d104b6a6b100a4f0757f
erosmartinelli <eros.martinelli@hotmail.it>
2023-03-28 23:51:06 +0200

    Finish chapter exercises

--- chapter15/Chapter15.hs
+++ chapter15/Chapter15.hs
@@ -1,10 +1,9 @@
-
 module Chapter15 () where
 
 import Test.QuickCheck
--------------------------
----Semigroup exercises---
--------------------------
+----------------
+---Semigroups---
+----------------
 
 ---Exercise 1---
 data Trivial = Trivial deriving (Eq, Show)
@@ -137,7 +136,7 @@ instance Semigroup (Or a b) where
   (Fst _) <> (Fst y) = Fst y
   (Fst _) <> (Snd y) = Snd y
 
----c'è un modo più elegante di scriverlo
+---c'è un modo più elegante di scriverlo?
 instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
   arbitrary =  do
     a <- arbitrary
@@ -153,26 +152,238 @@ checkOr = quickCheck (semigroupAssoc :: OrAssoc String String)
 
 newtype Combine a b = Combine { unCombine :: a -> b}
 
+---C'è un modo di scriverlo in maniera tale da ottenere show inc = \x -> x + 1?
 instance Show (Combine a b) where
   show _ = "just a function"
 
 instance (Semigroup b) => Semigroup (Combine a b) where
-  cf <> cg = Combine $ \n -> unCombine cf n <>  unCombine cg n
---coArbitrary perché Hom (-, b) è controvariante?
+  cf <> cg = Combine $ \a -> unCombine cf a <>  unCombine cg a
+--coArbitrary perché [-, b] è controvariante?
 instance (CoArbitrary a, Arbitrary b) => Arbitrary (Combine a b) where
   arbitrary = Combine <$> arbitrary
 
-compAssoc :: (Eq b, Semigroup b) => a
-                                    -> Combine a b
-                                    -> Combine a b
-                                    -> Combine a b
-                                    -> Bool
-compAssoc a cf cg ch = (unCombine $ (cf <> cg) <> ch) a ==
-                       (unCombine $ cf <> (cg <> ch)) a
+combineAssoc :: (Eq b, Semigroup b) => a
+                                       -> Combine a b
+                                       -> Combine a b
+                                       -> Combine a b
+                                       -> Bool
+combineAssoc a cf cg ch = (unCombine $ (cf <> cg) <> ch) a ==
+                          (unCombine $ cf <> (cg <> ch)) a
 
-type CombineAssoc a b = a -> Combine a b -> Combine a b -> Combine a b -> Bool
+type CombineAssoc a b = a
+                        -> Combine a b
+                        -> Combine a b
+                        -> Combine a b
+                        -> Bool
 
 checkCombine :: IO ()
-checkCombine = quickCheck (compAssoc :: CombineAssoc String String)
+checkCombine = quickCheck (combineAssoc :: CombineAssoc String String)
 
+---Exercise 10---
+
+newtype Comp a = Comp { unComp :: a -> a }
+
+instance Show (Comp a) where
+  show _ = "just an endomorphism"
+
+instance (Semigroup a) => Semigroup (Comp a) where
+  cf <> cg = Comp $ \a -> unComp cf a <> unComp cg a
+
+instance (CoArbitrary a, Arbitrary a) => Arbitrary (Comp a) where
+  arbitrary = Comp <$> arbitrary
+
+compAssoc :: (Eq a, Semigroup a) => a
+                                    -> Comp a
+                                    -> Comp a
+                                    -> Comp a
+                                    -> Bool
+compAssoc a cf cg ch = (unComp $ (cf <> cg) <> ch) a ==
+                       (unComp $ cf <> (cg <> ch)) a
+
+type CompAssoc a = a -> Comp a -> Comp a -> Comp a -> Bool
+
+checkComp :: IO ()
+checkComp = quickCheck (compAssoc :: CompAssoc String)
+
+---Exercise 11---
+---TODO: Ho letto male gli esercizi e sono partito per la tangente wrappando il datatype, rifattorizzare come da libro.
+
+data Validation a b = Failure' a | Success' b
+  deriving (Eq, Show)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Validation a b) where
+  arbitrary =  do
+        a <- arbitrary
+        b <- arbitrary
+        elements [Failure' a, Success' b]
+
+---First instance---
+newtype ValidationFst a b = ValidationFst {unValidationFst :: Validation a b}
+  deriving (Eq, Show)
+
+---Non mi piace, troppo boilerplate
+instance Semigroup (ValidationFst a b) where
+  vf <> vs = case unValidationFst vf of
+    (Failure' _) -> vf
+    (Success' _) -> case unValidationFst vs of
+                      (Failure' _) -> vs
+                      (Success' _) -> vf
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (ValidationFst a b) where
+  arbitrary = ValidationFst <$> (arbitrary :: Gen (Validation a b))
+
+type ValidationFstAssoc a b = ValidationFst a b
+                              -> ValidationFst a b
+                              -> ValidationFst a b
+                              -> Bool
+
+checkValidation :: IO ()
+checkValidation = quickCheck
+  (semigroupAssoc :: ValidationFstAssoc String String)
+
+---Exercise 12---
+
+---Second instance---
+newtype ValidationSnd a b = ValidationSnd {unValidationSnd :: Validation a b}
+  deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (ValidationSnd a b) where
+  vf <> vs = case (unValidationSnd vf, unValidationSnd vs) of
+               (Failure' _, Success' _) -> vf
+               (Success' _, Failure' _) -> vs
+               (Failure' x, Failure' y) -> ValidationSnd . Failure' $ (x <> y)
+               (Success' x, Success' y) -> ValidationSnd . Success' $ (x <> y)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (ValidationSnd a b) where
+  arbitrary = ValidationSnd <$> (arbitrary :: Gen (Validation a b))
+
+type ValidationSndAssoc a b = ValidationSnd a b
+                              -> ValidationSnd a b
+                              -> ValidationSnd a b
+                              -> Bool
+
+checkValidationSnd :: IO ()
+checkValidationSnd = quickCheck
+  (semigroupAssoc :: ValidationSndAssoc String String)
+
+-------------
+---Monoids---
+-------------
+monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
+
+---Exercise 1---
+
+instance Monoid Trivial where
+  mempty = Trivial
+  mappend = (<>)
+
+checkTrivialMon :: IO ()
+checkTrivialMon = do
+  quickCheck (semigroupAssoc :: TrivialAssoc)
+  quickCheck (monoidLeftIdentity :: Trivial -> Bool)
+  quickCheck (monoidRightIdentity :: Trivial -> Bool)
+
+---Exercise 2---
+
+instance (Monoid a) => Monoid (Identity a) where
+  mempty = Identity mempty
+  mappend = (<>)
+
+checkIdentityMon :: IO ()
+checkIdentityMon = do
+  quickCheck (semigroupAssoc :: IdentityAssoc String)
+  quickCheck (monoidLeftIdentity :: Identity String -> Bool)
+  quickCheck (monoidRightIdentity :: Identity String -> Bool)
+
+---Exercise 3---
+
+instance (Monoid a, Monoid b) => Monoid (Two a b) where
+  mempty = Two mempty mempty
+  mappend = (<>)
+
+checkTwoMon :: IO ()
+checkTwoMon = do
+  quickCheck (semigroupAssoc :: TwoAssoc String String)
+  quickCheck (monoidLeftIdentity :: Two String String -> Bool)
+  quickCheck (monoidRightIdentity :: Two String String -> Bool)
+
+---Exercise 4-5---
+---True e False sono gli elementi neutri.
+
+---Exercise 6---
+
+instance Monoid b => Monoid (Combine a b) where
+  mempty = Combine $ const mempty
+  mappend = (<>)
+
+monoidLeftIdentityCombine :: (Eq b, Monoid b) => a -> Combine a b -> Bool
+monoidLeftIdentityCombine a cf = unCombine (mempty <> cf) a == unCombine cf a
+
+monoidRightIdentityCombine :: (Eq b, Monoid b) => a -> Combine a b -> Bool
+monoidRightIdentityCombine a cf = unCombine (cf <> mempty) a == unCombine cf a
+
+checkCombineMon :: IO ()
+checkCombineMon = do
+  quickCheck (monoidLeftIdentityCombine :: String
+                                           -> Combine String String
+                                           -> Bool)
+  quickCheck (monoidRightIdentityCombine :: String
+                                            -> Combine String String
+                                            -> Bool)
+
+---Exercise 7---
+---Come sopra---
+
+---Exercise 8---
+newtype Mem s a = Mem {runMem :: s -> (a,s)}
+
+instance Show (Mem s a) where
+  show _ = "just a state transition"
+
+---fare un po' di golf coding giusto per prendere la mano?
+instance Semigroup a => Semigroup (Mem s a) where
+  sf <> sg = Mem $ \s -> ((fst . runMem sf $ s) <> (fst . runMem sg $ s)
+                         , snd . runMem sf . snd . runMem sg $ s)
+
+instance Monoid a => Monoid (Mem s a) where
+  mempty = Mem $ \s -> (mempty, s)
+  mappend = (<>)
+
+instance (CoArbitrary s, Arbitrary a, Arbitrary s) => Arbitrary (Mem s a) where
+  arbitrary = Mem <$> arbitrary
+
+---Come riutilizzare le istanze che ho scritto per Combine a b ?
+memAssoc :: (Eq a, Eq s, Semigroup a) => s
+                                         -> Mem s a
+                                         -> Mem s a
+                                         -> Mem s a
+                                         -> Bool
+memAssoc s cf cg ch = (runMem $ (cf <> cg) <> ch) s ==
+                      (runMem $ cf <> (cg <> ch)) s
+
+type MemAssoc s a = s
+                    -> Mem s a
+                    -> Mem s a
+                    -> Mem s a
+                    -> Bool
+
+monoidLeftIdentityMem :: (Eq a, Eq s, Monoid a) => s -> Mem s a -> Bool
+monoidLeftIdentityMem s cf = runMem (mempty <> cf) s == runMem cf s
+
+monoidRightIdentityMem :: (Eq a, Eq s, Monoid a) => s -> Mem s a -> Bool
+monoidRightIdentityMem s cf = runMem (cf <> mempty) s == runMem cf s
+
+checkMem :: IO ()
+checkMem = do
+  quickCheck (memAssoc :: MemAssoc String String)
+  quickCheck (monoidLeftIdentityMem :: String
+                                       -> Mem String String
+                                       -> Bool)
+  quickCheck (monoidRightIdentityMem :: String
+                                        -> Mem String String
+                                        -> Bool)
 
